db6169b602ce1b96fa495a8d092cd1720d588224
pete9991 <71692384+pete9991@users.noreply.github.com>
2024-10-03 12:43:57 +0200

    added no return error

--- errors.ml
+++ errors.ml
@@ -5,9 +5,11 @@ module TPretty = TypedPretty
 
 type error =
 | TypeMismatch of {expected : TAst.typ; actual : TAst.typ}
+| NoReturnError
 (* other errors to be added as needed. *)
 
 (* Useful for printing errors *)
 let error_to_string err =
   match err with
   | TypeMismatch {expected; actual; _} -> Printf.sprintf "Type mismatch: expected %s but found %s." (TPretty.typ_to_string expected) (TPretty.typ_to_string actual)
+  | NoReturnError -> Printf.sprintf "program is missing a return statement"
