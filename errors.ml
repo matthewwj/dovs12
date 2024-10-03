1ea294567ce4e23b7b5e545236a4ab4ceb89e4ab
Pedja <104004201+ItDoBeLikeThatSometimes@users.noreply.github.com>
2024-10-03 11:16:37 +0000

    Infertype_lval function done + new error type

--- errors.ml
+++ errors.ml
@@ -6,6 +6,7 @@ module TPretty = TypedPretty
 type error =
 | TypeMismatch of {expected : TAst.typ; actual : TAst.typ}
 | NoReturnError
+| UndeclaredVariable of { variablename : string }
 (* other errors to be added as needed. *)
 
 (* Useful for printing errors *)
@@ -13,3 +14,4 @@ let error_to_string err =
   match err with
   | TypeMismatch {expected; actual; _} -> Printf.sprintf "Type mismatch: expected %s but found %s." (TPretty.typ_to_string expected) (TPretty.typ_to_string actual)
   | NoReturnError -> Printf.sprintf "program is missing a return statement"
+  | UndeclaredVariable { variablename } -> Printf.sprintf "Undeclared variable %s" variablename
