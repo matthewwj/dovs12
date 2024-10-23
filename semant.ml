b9a88d89ae95cdbf6303c9d363ab463792ebf3b6
matthewwj <matthew.westergaard99@gmail.com>
2024-10-23 14:09:30 +0000

    mhm

--- semant.ml
+++ semant.ml
@@ -132,22 +132,6 @@ and typecheck_expr env expr tp =
 (* should check the validity of a statement and produce the corresponding typed statement. Should use typecheck_expr and/or infertype_expr as necessary. *)
 let rec typecheck_statement env (stm : Ast.statement) : TAst.statement * Env.environment =
   match stm with
-  (*| Ast.VarDeclStm (DeclBlock decls) -> 
-    let typechecked_decls, updated_env =
-      List.fold_left (fun (t_decls, current_env) decl ->
-      match decl with
-      | Ast.Declaration {name = Ast.Ident {name = sname}; tp; body} ->
-        let tname = TAst.Ident {sym = Sym.symbol sname} in
-        let typeBody, tbody_type =
-        match tp with
-          | None -> infertype_expr env body
-          | Some btp -> typecheck_expr env body (typecheck_typ btp), typecheck_typ btp
-        in
-    let new_decl = TAst.Declaration {name = tname; tp = tbody_type; body = typeBody} in
-    (new_decl :: t_decls, Env.insert_local_decl current_env (Sym.symbol sname) tbody_type)
-      ) ([], env) decls in
-    (TAst.VarDeclStm (DeclBlock (List.rev typechecked_decls)), updated_env)
-  *)
   | Ast.VarDeclStm (DeclBlock decls) -> 
     let typechecked_decls, updated_env =
       List.fold_left (fun (t_decls, current_env) decl ->
