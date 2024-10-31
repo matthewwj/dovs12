fd7e0b115dc6d44667c530d4c79e75be4251cbd1
Pedja <104004201+ItDoBeLikeThatSometimes@users.noreply.github.com>
2024-10-31 11:49:15 +0000

    Errors Updated.

--- errors.ml
+++ errors.ml
@@ -3,31 +3,49 @@ module Sym = Symbol
 module TAst = TypedAst
 module TPretty = TypedPretty
 
+exception ParseErr of string 
+
+let location_to_string (location : Location.location) =
+  let start_lnum = location.start_pos.pos_lnum in
+  let start_cnum = location.start_pos.pos_cnum in
+  let end_lnum = location.end_pos.pos_lnum in
+  let end_cnum = location.end_pos.pos_cnum in
+  let sentence =
+    Printf.sprintf
+      "start location (%d, %d) - end location (%d, %d)"
+      start_lnum
+      start_cnum
+      end_lnum
+      end_cnum
+  in
+  sentence
+;;
+
 type error =
-| TypeMismatch of {expected : TAst.typ; actual : TAst.typ}
+| TypeMismatch of {expected : TAst.typ; actual : TAst.typ; loc: Location.location}
 | NoReturnError
-| UndeclaredVariable of { variablename : string }
-| TooManyArguments of {expected: int ; actual: int}
-| MissingArguments of {expected: int; actual: int}
-| NotAFunction of {name : string}
-| UnexpectedReturnType of { expected : TAst.typ; actual : TAst.typ}
-| Shadowing of {name : string} 
-| NotEndInRet
-| UnexpectedBreak
-| UnexpectedContinue
+| UndeclaredVariable of { variablename : string; loc: Location.location }
+| TooManyArguments of {expected: int ; actual: int; loc: Location.location}
+| MissingArguments of {expected: int; actual: int; loc: Location.location}
+| NotAFunction of {name : string; loc: Location.location}
+| UnexpectedReturnType of { expected : TAst.typ; actual : TAst.typ; loc: Location.location}
+| Shadowing of {name : string; loc: Location.location} 
+| NotEndInRet 
+| UnexpectedBreak of {loc: Location.location}
+| UnexpectedContinue of {loc: Location.location}
 (* other errors to be added as needed. *)
 
 (* Useful for printing errors *)
 let error_to_string err =
   match err with
-  | TypeMismatch {expected; actual; _} -> Printf.sprintf "Type mismatch: expected %s but found %s." (TPretty.typ_to_string expected) (TPretty.typ_to_string actual)
+  | TypeMismatch {expected; actual; loc} -> Printf.sprintf "Type mismatch: expected %s but found %s ar %s." (TPretty.typ_to_string expected) (TPretty.typ_to_string actual) (location_to_string loc)
   | NoReturnError -> Printf.sprintf "program is missing a return statement"
-  | UndeclaredVariable { variablename } -> Printf.sprintf "Undeclared variable %s" variablename
-  | TooManyArguments {expected; actual} -> Printf.sprintf "Too many arguments: expected %d but found %d." expected actual
-  | MissingArguments {expected; actual} -> Printf.sprintf "Too few arguments: expected %d but found %d." expected actual
-  | NotAFunction {name} -> Printf.sprintf "Tried to call non-function %s" name 
-  | UnexpectedReturnType {expected; actual} -> Printf.sprintf "Unexpected return type: expected %s but found %s." (TPretty.typ_to_string expected) (TPretty.typ_to_string actual)
+  | UndeclaredVariable { variablename; loc } -> Printf.sprintf "Undeclared variable %s at %s" variablename (location_to_string loc)
+  | TooManyArguments {expected; actual; loc} -> Printf.sprintf "Too many arguments: expected %d but found %d at %s." expected actual (location_to_string loc)
+  | MissingArguments {expected; actual; loc} -> Printf.sprintf "Too few arguments: expected %d but found %d at %s." expected actual (location_to_string loc)
+  | NotAFunction {name; loc} -> Printf.sprintf "Tried to call non-function %s at %s" name (location_to_string loc) 
+  | UnexpectedReturnType {expected; actual; loc} -> Printf.sprintf "Unexpected return type: expected %s but found %s at %s." (TPretty.typ_to_string expected) (TPretty.typ_to_string actual) (location_to_string loc)
   | NotEndInRet -> "The program does not end in a return statement."
-  | Shadowing {name} -> Printf.sprintf "Shadowing error please update this error msg %s" name 
-  | UnexpectedBreak -> Printf.sprintf "Break not in loop"
-  | UnexpectedContinue -> Printf.sprintf "Continue not in loop"
+  | Shadowing {name; loc} -> Printf.sprintf "Shadowing error please update this error msg %s at %s" name (location_to_string loc) 
+  | UnexpectedBreak {loc} -> Printf.sprintf "Break not in loop at %s" (location_to_string loc)
+  | UnexpectedContinue {loc} -> Printf.sprintf "Continue not in loop at %s" (location_to_string loc)
