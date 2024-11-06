fd3cf099a337788f4b42d1f043d0a71bfc6dc93e
mjezek20 <73278473+mjezek20@users.noreply.github.com>
2024-11-06 12:02:45 +0100

    small changes

--- codegen.ml
+++ codegen.ml
@@ -301,33 +301,3 @@ let codegen_prog tprog=
 
 
 
-let write_to_file (path: string) (contents: string) = 
-  (* Create the full path with the 'testfiles' directory *)
-  let dir = "llvm_outputs" in
-  let full_path = Filename.concat dir path in
-
-  (* Ensure the 'testfiles' directory exists, create it if it doesn't *)
-  if not (Sys.file_exists dir) then Sys.mkdir dir 0o755;
-
-  (* Write the file to the 'llvm_outputs' directory *)
-  let oc = open_out full_path in
-  Printf.fprintf oc "%s" contents;
-  close_out oc
-
-let compile_prog program =
-  try
-    let typedStmt, _ = Semant.typecheck_prog program in
-    let llvm_prog = codegen_prog typedStmt in
-    let llvm_ir_string = Ll.string_of_prog llvm_prog in
-    print_endline llvm_ir_string;
-    exit 0
-  with
-  | Invalid_argument msg ->
-    prerr_endline ("Typecheck failed: " ^ msg);
-    exit 1
-  | Unimplemented ->
-    prerr_endline "Unimplemented feature encountered.";
-    exit 1
-  | _ ->
-    prerr_endline "Unknown error encountered.";
-    exit 1
--- main.ml
+++ main.ml
@@ -1,37 +1,27 @@
 module Errors = Errors
 module Codegen = Codegen
 module Semant = Semant
-(*
-let _ = 
-  let file_in = open_in "/workspaces/dovs/phase1/dovs12/input.txt"
- in 
-  let lex_buf = Lexing.from_channel file_in in 
-  let parse_res = Parser.main Lexer.read lex_buf in
-  PrintBox_text.output stdout (Pretty.program_to_tree parse_res)*)
 
-(*let parse_from_file ?(with_positions = true) file_name =
-  let file_in = open_in file_name in
-  let lex_buf = Lexing.from_channel ~with_positions file_in in
-  try Parser.main Lexer.read lex_buf with
-  | Parser.Error ->
-    let c = lex_buf.lex_curr_p.pos_cnum - lex_buf.lex_curr_p.pos_bol in
-    let err =
-      "Syntax Error, Line "
-      ^ string_of_int lex_buf.lex_curr_p.pos_lnum
-      ^ ", coloum "
-      ^ string_of_int c
-      ^ ", error in: \""
-      ^ Bytes.sub_string lex_buf.lex_buffer lex_buf.lex_curr_p.pos_bol c
-      ^ "\""
-    in
-    raise @@ Errors.ParseErr err
-;;*)
+let write_to_file (path: string) (contents: string) = 
+  (* Create the full path with the 'testfiles' directory *)
+  let dir = "llvm_outputs" in
+  let full_path = Filename.concat dir path in
+
+  (* Ensure the 'testfiles' directory exists, create it if it doesn't *)
+  if not (Sys.file_exists dir) then Sys.mkdir dir 0o755;
+
+  (* Write the file to the 'llvm_outputs' directory *)
+  let oc = open_out full_path in
+  Printf.fprintf oc "%s" contents;
+  close_out oc
+
+
 let compile_prog pathtofile = 
   let file_in = open_in pathtofile in 
   let lex_buf = Lexing.from_channel file_in in 
   let parse_res = Parser.main Lexer.read lex_buf in
-  let (typedStmt, env) = Semant.typecheck_prog parse_res in
-  (*PrintBox_text.output stdout (Pretty.program_to_tree parse_res);*)
+  let (typedStmt, _) = Semant.typecheck_prog parse_res in
+  (*PrintBox_text.output stdout (Pretty.program_to_tree parse_res);*)  (* For printing parsetree*)
   let llvm_prog = Codegen.codegen_prog typedStmt in
   let llvm_ir_string = Ll.string_of_prog llvm_prog in
   print_endline llvm_ir_string;
